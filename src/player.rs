3ebe4edaf4c4789442afc8a5379735e249c347fd
S <smonfourny@gmail.com>
2021-01-15 15:58:53 -0500

    feat: allow player to run into enemies to attack

--- src/player.rs
+++ src/player.rs
@@ -10,12 +10,27 @@ fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
     let mut viewshed = ecs.write_storage::<Viewshed>();
+    let combat_stats = ecs.read_storage::<CombatStats>();
     let map = ecs.fetch::<Map>();
 
     for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewshed).join() {
-        if !map.blocked[(pos.x + delta_x) as usize][(pos.y + delta_y) as usize] {
-            pos.x = min(MAP_X - 1, max(0, pos.x + delta_x));
-            pos.y = min(MAP_Y - 1, max(0, pos.y + delta_y));
+        let destination_x = pos.x + delta_x;
+        let destination_y = pos.y + delta_y;
+
+        for potential_target in map.tile_content[destination_x as usize][destination_y as usize].iter() {
+            let target = combat_stats.get(*potential_target);
+            match target {
+                None => {},
+                Some(t) => {
+                    console::log(&format!("HIYAA"));
+                    return;
+                }
+            }
+        }
+
+        if !map.blocked[destination_x as usize][destination_y as usize] {
+            pos.x = min(MAP_X - 1, max(0, destination_x));
+            pos.y = min(MAP_Y - 1, max(0, destination_y));
 
             let mut ppos = ecs.write_resource::<Point>();
             ppos.x = pos.x;
