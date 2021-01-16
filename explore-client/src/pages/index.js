e8429001db37065583dc29615e55a4318ba87f7f
S <smonfourny@gmail.com>
2021-01-16 15:41:19 -0500

    feat(client): update command list on client page

--- explore-client/src/pages/index.js
+++ explore-client/src/pages/index.js
@@ -8,7 +8,11 @@ class IndexPage extends React.Component {
         <title>Explore</title>
         <div className="bg-gray-200 h-screen grid place-content-center">
           <p className="px-6 font-bold">Welcome to Explore, traveller!</p>
-          <p className="px-6 font-light">Press the arrow keys to move your character.</p>
+          <p className="px-6 font-light mb-4">Press the arrow keys to move your character.</p>
+          <ul>
+            <li className="px-6"><strong>g</strong> - Pick up item</li>
+            <li className="px-6"><strong>i</strong> - Open inventory</li>
+          </ul>
           <div className="p-6 flex justify-center">
             <div className="flex-shrink-0 shadow-2xl">
               <canvas id="canvas" width="640" height="480" className="rounded-lg"></canvas>
